0647ff84f98caf1f8235fcd71d029350b83df1e4
MiSalah <elbaidoury.salaheddine@gmail.com>
2021-12-04 13:28:14 +0100

    split the dataset to training dataset & test dataset

--- Simple Linear Regression/salary_data_regression.py
+++ Simple Linear Regression/salary_data_regression.py
@@ -14,5 +14,13 @@ dataset = pd.read_csv("Salary_data.csv")
 # x : the dependent variable -> salary
 # y : the independent variable -> years experience
 
-x = dataset.iloc[:, :-1].values
+X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 1].values
+
+# split the data into training dataset and test dataset
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+
