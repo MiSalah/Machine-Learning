d21053a031dc81bdfc22972dadcbcfc55450080f
MiSalah <elbaidoury.salaheddine@gmail.com>
2021-12-04 13:17:22 +0100

    load the dataset

---
+++ Simple Linear Regression/salary_data_regression.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Load the dataset
+
+dataset = pd.read_csv("Salary_data.csv")
+
+
