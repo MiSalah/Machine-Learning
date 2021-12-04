f5cf72198c1725a26ed4c3ae634db7a4d6d7e2dd
MiSalah <elbaidoury.salaheddine@gmail.com>
2021-12-04 13:51:34 +0100

    fit & visualisation

--- Simple Linear Regression/salary_data_regression.py
+++ Simple Linear Regression/salary_data_regression.py
@@ -20,7 +20,31 @@ y = dataset.iloc[:, 1].values
 # split the data into training dataset and test dataset
 
 from sklearn.model_selection import train_test_split
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
+# fit the simple linear regression 
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+# predict the dataset test
+
+y_pred = regressor.predict(X_test) # salary = f(years of experience)
+
+# Visualise the training set 
+plt.scatter(X_train, y_train, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'green')
+plt.title('Salary vs Experience (Training set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+# Visualising the Test set results
+plt.scatter(X_test, y_test, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'green')
+plt.title('Salary vs Experience (Test set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
 
